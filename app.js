ddf0296266284b5336e1c5194ea2fe97c898084a
sunwucode <wukongkhan8@gmail.com>
2019-07-29 18:07:11 +0200

    first part of authentication - sign up

--- app.js
+++ app.js
@@ -3,22 +3,54 @@ const express = require("express");
 const app = express();
 // let ejs = require('ejs');
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose")
+var mongoose = require("mongoose");
+passport = require("passport");
+localStrategy = require("passport-local");
 Campground = require("./models/campground");
-seedDB = require("./seed");
+User = require("./models/user");
+// seedDB = require("./seed");
+// seedDB();
 
-seedDB();
 //APP CONFIG//
 mongoose.connect("mongodb://localhost/blog_practice")
-app.use(bodyParser.urlencoded({useNewUrlParser: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 app.use(express.static("public"));
 
+//Passport configuration //
 
+app.use(require("express-session")({
+  secret: "I hate cats",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
+//==============
+// AUTH ROUTES
+//==============
 
-
-
+//show register form
+app.get("/register", (req, res)=> {
+  res.render("register");
+})
+// handle sign up logic
+app.post("/register", (req, res) => {
+  var newUser = ({username: req.body.username});
+  User.register(newUser, req.body.password, function (err, user) {
+    if(err){
+      console.log(err);
+      return res.render("register")
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    });
+  });
+});
 
 app.get('/', (request, response, next) => {
   response.render("landing");
